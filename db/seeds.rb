d5c88c38499018ba8a1595c3a8b7b53fae10c4df
jacobjohn90 <jacobjohn90@gmail.com>
2018-07-24 18:20:22 -0400

    created seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Comment.destroy_all
+Movie.destroy_all
+User.destroy_all
+
+users = User.create!([
+    {
+        email: 'test@tester.com',
+        password: 'testtest',
+        password_confirmation: 'testtest'
+    },
+    {
+        email: 'jacob@john.com',
+        password: 'testtest',
+        password_confirmation: 'testtest'
+    },
+    {
+        email: 'example@example.com',
+        password: 'testtest',
+        password_confirmation: 'testtest'
+    }
+
+])
+
+movies = Movie.create!([
+    {
+        title: 'Test',
+        mpaa_rating: 'G',
+        img: 'www.google.com',
+        summary: 'asd;faj; foaejf;aosfjva;osjva;sja;ffjas;ofijas;foiasj ;aosijf a;soifjas;vja s;vja s;oivaj sd;ofiaj sdf;oiaj f;'
+
+    },
+        {
+        title: 'Best Movie',
+        mpaa_rating: 'R',
+        img: 'www.google.com',
+        summary: 'asd;faj; foaejf;aosfjva;osjva;sja;ffjas;ofijas;foiasj ;aosijf a;soifjas;vja s;vja s;oivaj sd;ofiaj sdf;oiaj f;'
+
+    }
+])
+
+Comment.create!(user_id: users[0].id, movie_id: movies[0].id, :content=>'this movie was great!')
+Comment.create!(user_id: users[0].id, movie_id: movies[1].id, :content=>'meh')
+
+Comment.create!(user_id: users[1].id, movie_id: movies[0].id, :content=>'it was okay')
+Comment.create!(user_id: users[1].id, movie_id: movies[1].id, :content=>'this movie was great!')
+
+Comment.create!(user_id: users[2].id, movie_id: movies[0].id, :content=>'wow, amazing!')
+Comment.create!(user_id: users[2].id, movie_id: movies[1].id, :content=>'save your money')
