b5994c9d968186d4ee6260d1799f0acee777f90f
jacobjohn90 <jacobjohn90@gmail.com>
2018-07-25 11:57:45 -0400

    sign out function is functional

--- client/src/components/MainPage.js
+++ client/src/components/MainPage.js
@@ -2,30 +2,49 @@ import React, { Component } from 'react';
 import SignUp from './SignUp';
 import SignIn from './SignIn';
 import Movies from './Movies';
-import { userIsLoggedIn, setAxiosDefaults } from '../util/SessionHeaderUtil';
+import { userIsLoggedIn, setAxiosDefaults, clearAuthTokens } from '../util/SessionHeaderUtil';
+import Axios from '../../node_modules/axios';
 
 class MainPage extends Component {
     state = {
         signedIn: false
     }
 
-    async componentDidMount(){
+    componentDidMount() {
         const signedIn = userIsLoggedIn()
         if (signedIn) {
             setAxiosDefaults()
         }
-        this.setState({signedIn})
+        this.setState({ signedIn })
     }
 
     updateSignedIn = () => {
-        this.setState({signedIn: true})
+        this.setState({ signedIn: true })
+    }
+
+    signOut = async (event) => {
+        event.preventDefault()
+        try {
+            await Axios.delete('/auth/sign_out')
+            clearAuthTokens()
+            this.setState({ signedIn: false })
+        } catch (error) {
+            console.error(error)
+        }
     }
 
     render() {
         return (
             <div>
-                <SignUp updateSignedIn={this.updateSignedIn} />
-                <SignIn updateSignedIn={this.updateSignedIn} />
+                {this.state.signedIn
+                    ?
+                    <button onClick={this.signOut}>Sign Out</button>
+                    :
+                    <div>
+                        <SignUp updateSignedIn={this.updateSignedIn} />
+                        <SignIn updateSignedIn={this.updateSignedIn} />
+                    </div>
+                }
                 <Movies />
             </div>
         );
