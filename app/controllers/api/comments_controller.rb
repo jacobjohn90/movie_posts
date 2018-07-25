7dae54092d78da416abf61a9b1b3e4b1c23ecb3d
jacobjohn90 <jacobjohn90@gmail.com>
2018-07-25 18:19:20 -0400

    show comments when not logged in

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,5 +1,5 @@
 class Api::CommentsController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, except: [:show, :index]
     load_and_authorize_resource only: [:destroy]
 
     def index
