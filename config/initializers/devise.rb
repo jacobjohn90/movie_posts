72b1a93ada0957238582e0ddf718673ed01f9646
jacobjohn90 <jacobjohn90@gmail.com>
2018-07-27 14:13:03 -0400

    added devise.rb file in initializer

---
+++ config/initializers/devise.rb
@@ -0,0 +1,3 @@
+Devise.setup do |config|
+    config.secret_key = 'a1427fff9c668b90c8eeb2465a1c68c285f203252a221dea2adef65bdab9c6d58738b5cf10b4600436c74ae134e7be2630d0bc25b2953fb55499b29a73910670'
+end
