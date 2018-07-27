ca2812e1ad3467b4e504b95cc9654cbe0ab7aa0a
jacobjohn90 <jacobjohn90@gmail.com>
2018-07-27 12:09:07 -0400

    able to edit username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,6 +12,7 @@ class ApplicationController < ActionController::API
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
   end
   
 end
