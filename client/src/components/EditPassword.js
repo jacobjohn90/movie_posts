2bf635832813db998d4d5dc76e1eb06a6bbda545
jacobjohn90 <jacobjohn90@gmail.com>
2018-07-27 20:42:21 -0400

    update password is functional on browser

---
+++ client/src/components/EditPassword.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { setAxiosDefaults } from '../util/SessionHeaderUtil';
+
+class EditPassword extends Component {
+    state = {
+        password: '',
+        password_confirmation: '',
+        currentUser: {}
+    }
+
+    async componentDidMount() {
+        const currentUser = await this.fetchUser()
+        this.setState({ currentUser })
+    }
+
+    fetchUser = async () => {
+        const userId = this.props.match.params.user_id
+        try {
+            setAxiosDefaults()
+            const res = await axios.get(`/api/users/${userId}`)
+            return res.data
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    handleChange = (event) => {
+        const userInput = event.target.value
+        const inputName = event.target.name
+        const newState = { ...this.state }
+        newState[inputName] = userInput
+        this.setState(newState)
+    }
+
+    handleSubmit = async (event) => {
+        event.preventDefault()
+        const payload = {
+            password: this.state.password,
+            password_confirmation: this.state.password_confirmation
+
+        }
+        setAxiosDefaults()
+        const res = await axios.patch('/auth/password', payload)
+        console.log(res.data.data)
+        this.setState({ currentUser: res.data.data })
+        this.props.updateUser(res.data.data)
+        this.props.handleUpdateShow('password')
+    }
+    handleCancel = (event) => {
+        event.preventDefault()
+        this.props.handleUpdateShow('password')
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label>New Password: </label>
+                        <input type="text" name="password" value={this.state.password} onChange={this.handleChange} />
+                    </div>
+                    <div>
+                        <label>Confirm New Password: </label>
+                        <input type="text" name="password_confirmation" value={this.state.password_confirmation} onChange={this.handleChange} />
+                    </div>
+                    <button type="submit">Save</button>
+                </form>
+                <button onClick={this.handleCancel}>Cancel</button>
+            </div>
+        );
+    }
+}
+
+export default EditPassword;
